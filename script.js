2a71ad7682e0e5ad6a8f3eb33d8a3eedf95291d5
Debasishkaity22 <139854092+Debasishkaity22@users.noreply.github.com>
2023-12-06 12:03:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+let count=0;
+let counter1=document.getElementById("counter");
+counter1.textContent=count;
+function update(){
+    alert(count);
+	count++;
+	counter1.textContent=count;
+}
